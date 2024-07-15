d4cc6cae2b94f73ad0291235127da3f6b045f858
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-07-15 18:41:29 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSimple } from './hooks/useSimple';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 function App() {
-  const [text, setText, logText] = useSimple('');
+  const navigate = useNavigate();
   return (
     <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={logText}>See Value</button>
+      <div>
+        <button onClick={() => navigate('/')}>Name Form</button>
+        <button onClick={() => navigate('/cars')}>Car Form</button>
+      </div>
+      <Routes>
+        <Route path="/" element={<div>Name Form</div>} />
+        <Route path="/cars" element={<div>Car Form</div>} />
+      </Routes>
+      <p>something else</p>
     </div>
   );
 }
